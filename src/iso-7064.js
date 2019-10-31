7a98e212620d0b0f963272a4de299f84ab486bc7
Etienne Dumont <etienne.g.dumont@gmail.com>
2019-10-31 14:58:42 +0100

    Fix codacy issues (2)

--- src/iso-7064.js
+++ src/iso-7064.js
@@ -1,5 +1,39 @@
 'use strict';
 
+const CHARCODE_A = 'A'.charCodeAt(0);
+const CHARCODE_0 = '0'.charCodeAt(0);
+
+const FORMAT = /^[0-9A-Z]{1,}$/;
+
+function mod97(value) {
+    var buffer = 0;
+    var charCode;
+
+    for (var i = 0; i < value.length; ++i) {
+        charCode = value.charCodeAt(i);
+
+        buffer = charCode + (charCode >= CHARCODE_A ? buffer * 100 - CHARCODE_A + 10 : buffer * 10 - CHARCODE_0);
+
+        if (buffer > 1000000) {
+            buffer %= 97;
+        }
+    }
+
+    return buffer % 97;
+}
+
+function stringifyInput(rawValue) {
+    if (rawValue === null || rawValue === undefined) {
+        throw new Error('Expecting \'rawValue\' of type \'string\', found: \'' + rawValue + '\'');
+    }
+
+    if (typeof rawValue !== 'string') {
+        throw new Error('Expecting \'rawValue\' of type \'string\', found: \'' + (typeof rawValue) + '\'');
+    }
+
+    return rawValue;
+}
+
 var iso7064 = {
     /**
      * Check requirements.
@@ -37,39 +71,5 @@ var iso7064 = {
         return mod97(rawValue);
     }
 };
-
-const CHARCODE_A = 'A'.charCodeAt(0);
-const CHARCODE_0 = '0'.charCodeAt(0);
-
-const FORMAT = /^[0-9A-Z]{1,}$/;
-
-function mod97(value) {
-    var buffer = 0;
-    var charCode;
-
-    for (var i = 0; i < value.length; ++i) {
-        charCode = value.charCodeAt(i);
-
-        buffer = charCode + (charCode >= CHARCODE_A ? buffer * 100 - CHARCODE_A + 10 : buffer * 10 - CHARCODE_0);
-
-        if (buffer > 1000000) {
-            buffer %= 97;
-        }
-    }
-
-    return buffer % 97;
-}
-
-function stringifyInput(rawValue) {
-    if (rawValue === null || rawValue === undefined) {
-        throw new Error('Expecting \'rawValue\' of type \'string\', found: \'' + rawValue + '\'');
-    }
-
-    if (typeof rawValue !== 'string') {
-        throw new Error('Expecting \'rawValue\' of type \'string\', found: \'' + (typeof rawValue) + '\'');
-    }
-
-    return rawValue;
-}
 
 module.exports = iso7064;
