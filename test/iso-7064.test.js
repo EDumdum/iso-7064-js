f46f2e7858d5fc8edcbd0043f5e2dc3114de0491
DUMONT Etienne <etienne.dumont@htree.be>
2022-06-06 13:17:25 +0200

    Convert test to jest

---
+++ test/iso-7064.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const iso7064 = require('../src/iso-7064');
+
+test('iso-7064.compute - invalid parameter type', () => {
+    expect(() => iso7064.compute(null)).toThrow('Expecting \'rawValue\' of type \'string\', found: \'null\'');
+    expect(() => iso7064.compute(undefined)).toThrow('Expecting \'rawValue\' of type \'string\', found: \'undefined\'');
+    expect(() => iso7064.compute([])).toThrow('Expecting \'rawValue\' of type \'string\', found: \'object\'');
+});
+
+test('iso-7064.compute - invalid parameter format', () => {
+    expect(() => iso7064.compute('')).toThrow('Invalid data format; expecting: \'/^[0-9A-Z]{1,}$/\', found: \'\'');
+    expect(() => iso7064.compute('012?456')).toThrow('Invalid data format; expecting: \'/^[0-9A-Z]{1,}$/\', found: \'012?456\'');
+    expect(() => iso7064.compute('abc')).toThrow('Invalid data format; expecting: \'/^[0-9A-Z]{1,}$/\', found: \'abc\'');
+    expect(() => iso7064.compute('969500T3M-BS4SQAMHJ4A')).toThrow('Invalid data format; expecting: \'/^[0-9A-Z]{1,}$/\', found: \'969500T3M-BS4SQAMHJ4A\'');
+});
+
+test('iso-7064.compute - valid  parameter', () => {
+    expect(iso7064.compute('969500T3M8S4SQAMHJ')).toBe(50);
+    expect(iso7064.compute('969500KSV493XWY0PS')).toBe(54);
+    expect(iso7064.compute('7245005WBNJAFHBD0S')).toBe(55);
+    expect(iso7064.compute('724500VKKSH9QOLTFR')).toBe(38);
+    expect(iso7064.compute('724500884QS64MG71N')).toBe(76);
+    expect(iso7064.compute('724500884QS64MG71N64')).toBe(1);
+});
+
+test('iso-7064.computeWithoutCheck - invalid parameter type', () => {
+    expect(() => iso7064.computeWithoutCheck(null)).toThrow('Cannot read properties of null (reading \'length\')');
+    expect(() => iso7064.computeWithoutCheck(undefined)).toThrow('Cannot read properties of undefined (reading \'length\')');
+    expect(iso7064.computeWithoutCheck([])).toBe(0);
+});
+
+test('iso-7064.computeWithoutCheck - invalid parameter format (no error but funny result)', () => {
+    expect(iso7064.computeWithoutCheck('')).toBe(0);
+    expect(iso7064.computeWithoutCheck('012?456')).toBe(44);
+    expect(iso7064.computeWithoutCheck('abc')).toBe(66);
+    expect(iso7064.computeWithoutCheck('969500T3M-BS4SQAMHJ4A')).toBe(78);
+});
+
+test('iso-7064.computeWithoutCheck - valid parameter', () => {
+    expect(iso7064.computeWithoutCheck('969500T3M8S4SQAMHJ')).toBe(50);
+    expect(iso7064.computeWithoutCheck('969500KSV493XWY0PS')).toBe(54);
+    expect(iso7064.computeWithoutCheck('7245005WBNJAFHBD0S')).toBe(55);
+    expect(iso7064.computeWithoutCheck('724500VKKSH9QOLTFR')).toBe(38);
+    expect(iso7064.computeWithoutCheck('724500884QS64MG71N')).toBe(76);
+    expect(iso7064.computeWithoutCheck('724500884QS64MG71N64')).toBe(1);
+});
