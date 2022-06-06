fa189cd8ceb99e770eb6690f30d838fe4c0a3d66
DUMONT Etienne <etienne.dumont@htree.be>
2022-06-06 13:29:40 +0200

    Review mod97 + add tests

--- src/iso-7064.js
+++ src/iso-7064.js
@@ -1,25 +1,41 @@
 'use strict';
 
-const CHARCODE_A = 'A'.charCodeAt(0);
 const CHARCODE_0 = '0'.charCodeAt(0);
+const CHARCODE_0_MULTIPLIER = 10;
+const CHARCODE_0_OFFSET = CHARCODE_0;
+const CHARCODE_A = 'A'.charCodeAt(0);
+const CHARCODE_A_MULTIPLIER = 100;
+const CHARCODE_A_OFFSET = CHARCODE_A - 10;
+
+const BUFFER_INITIAL_VALUE = 0;
+const MODULATOR_97 = 97;
 
 const FORMAT = /^[0-9A-Z]{1,}$/;
 
 function mod97(value) {
-    var buffer = 0;
-    var charCode;
+    const length = value.length;
+    var buffer = BUFFER_INITIAL_VALUE;
 
-    for (var i = 0; i < value.length; ++i) {
-        charCode = value.charCodeAt(i);
+    for (var i = 0; i < length; ++i) {
+        const charCode = value.charCodeAt(i);
+        let multiplier;
+        let offset;
 
-        buffer = charCode + (charCode >= CHARCODE_A ? buffer * 100 - CHARCODE_A + 10 : buffer * 10 - CHARCODE_0);
-
-        if (buffer > 1000000) {
-            buffer %= 97;
+        if (charCode >= CHARCODE_A) {
+            multiplier = CHARCODE_A_MULTIPLIER;
+            offset = CHARCODE_A_OFFSET;
+        } else {
+            multiplier = CHARCODE_0_MULTIPLIER;
+            offset = CHARCODE_0_OFFSET;
         }
+
+        buffer *= multiplier;
+        buffer -= offset;
+        buffer += charCode;
+        buffer %= MODULATOR_97;
     }
 
-    return buffer % 97;
+    return buffer;
 }
 
 function stringifyInput(rawValue) {
